8c657cb2c62be123a9030ed383b5c58b1d1bde88
Jeffrey Yau <jeffreyyau@outlook.com>
2018-12-14 20:55:09 +0800

    style: Fix indentation and newline at end of file

--- js/app.js
+++ js/app.js
@@ -2,22 +2,22 @@
  * Create a list that holds all of your cards
  */
 let cardList = [
-    'fa-anchor',
-    'fa-anchor',
-    'fa-bicycle',
-    'fa-bicycle',
-    'fa-bolt',
-    'fa-bolt',
-    'fa-bomb',
-    'fa-bomb',
-    'fa-cube',
-    'fa-cube',
-    'fa-diamond',
-    'fa-diamond',
-    'fa-leaf',
-    'fa-leaf',
-    'fa-paper-plane-o',
-    'fa-paper-plane-o',
+	'fa-anchor',
+	'fa-anchor',
+	'fa-bicycle',
+	'fa-bicycle',
+	'fa-bolt',
+	'fa-bolt',
+	'fa-bomb',
+	'fa-bomb',
+	'fa-cube',
+	'fa-cube',
+	'fa-diamond',
+	'fa-diamond',
+	'fa-leaf',
+	'fa-leaf',
+	'fa-paper-plane-o',
+	'fa-paper-plane-o',
 ];
 
 /*
@@ -30,43 +30,43 @@ let deck = document.getElementById('deck');
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-    return array;
+	var currentIndex = array.length, temporaryValue, randomIndex;
+	while (currentIndex !== 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+	return array;
 }
 
 // Dynamically generates and randomly shuffles cards
 function generateCards() {
-    let shuffledCards = shuffle(cardList);
-    for(card of shuffledCards) {
-        let li = document.createElement('li');
-        let i = document.createElement('i');
-        li.classList.add('card');
-        i.classList.add('fa', card)
-        li.appendChild(i);
-        deck.appendChild(li);
-    }
+	let shuffledCards = shuffle(cardList);
+	for (card of shuffledCards) {
+		let li = document.createElement('li');
+		let i = document.createElement('i');
+		li.classList.add('card');
+		i.classList.add('fa', card)
+		li.appendChild(i);
+		deck.appendChild(li);
+	}
 }
 
 // card flips on click
 function flipCard() {
-    deck.addEventListener('click', function() {
-        if (event.target.classList.contains('card')) {
-            event.target.classList.toggle('open');
-            event.target.classList.toggle('show');
-        }
-    });
+	deck.addEventListener('click', function () {
+		if (event.target.classList.contains('card')) {
+			event.target.classList.toggle('open');
+			event.target.classList.toggle('show');
+		}
+	});
 }
 
 function startGame() {
-    generateCards();
-    flipCard();
+	generateCards();
+	flipCard();
 }
 
 /*
