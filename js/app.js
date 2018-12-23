5b09e098c30b874158c1c5e94cd14cd92c934d1e
Jeffrey Yau <jeffreyyau@outlook.com>
2018-12-23 09:19:02 +0800

    feat: Add game modal function
    This commit add a game scrore modal which will show up after all sixteen cards have been flipped up, the replay button on the game modal restart the game on click; the modal also display moves, star counts, time used, and a congratulation message.

--- js/app.js
+++ js/app.js
@@ -30,6 +30,9 @@ let time = 0;
 let watchId;
 let watchOff = true;
 
+let matches = 0;
+let allMatches = 8;
+
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
 	var currentIndex = array.length, temporaryValue, randomIndex;
@@ -63,7 +66,7 @@ function toggleCard(cardToToggle) {
 }
 
 // cards listen to click events
-function initClick() {
+function initCards() {
 	deck.addEventListener('click', () => {
 		let clickedCard = event.target;
 		if ( // proceed if cards meet these conditions
@@ -96,6 +99,10 @@ function checkMatching() {
 		openedCards[0].classList.add('match');
 		openedCards[1].classList.add('match');
 		openedCards = [];
+		matches++;
+		if (matches === allMatches) {
+			gameWon();
+		}
 	} else {
 		setTimeout(() => {
 			toggleCard(openedCards[0]);
@@ -132,11 +139,11 @@ function starCounter() {
 function stopwatch() {
 	watchId = setInterval(() => {
 		time++;
-		watchValue()
+		watchValue();
 	}, 1000);
 }
 
-function watchValue() {
+function watchValue() { // convert raw time value into stopwatch format
 	let minutes = Math.floor(time / 60);
 	let seconds = time % 60;
 	(minutes < 10 && seconds < 10) ? (watchText.innerText = `0${minutes}:0${seconds}`)
@@ -145,7 +152,7 @@ function watchValue() {
 	: (watchText.innerText = `${minutes}:${seconds}`);
 }
 
-// return the game to initial state
+// reset all game stats
 function resetGameState() {
 	// reset game board
 	deck.innerHTML = '';
@@ -163,17 +170,61 @@ function resetGameState() {
 	time = 0;
 	watchOff = true;
 	watchValue();
+	// reset match counts
+	matches = 0;
+}
+
+// return game to initial state
+function restartButton() {
+	let restartBtn = document.querySelector('.restart');
+	restartBtn.addEventListener('click', resetGameState);
+}
+
+// fill game modal with values of moves, satrs, and stopwatch time
+function fillModalContent() {
+	let moveStat = document.querySelector('.modal-moves');
+	let starStat = document.querySelector('.modal-stars');
+	let timeStat = document.querySelector('.modal-time');
+	let watchTime = watchText.innerText;
+	let starCount = 0;
+	for (star of starList) {
+		if (!star.classList.contains('hide')) {
+			starCount++;
+		}
+	}
+	timeStat.innerText = `Time: ${watchTime}`;
+	moveStat.innerText = `Moves: ${moves}`;
+	starStat.innerText = `Stars: ${starCount}`;
+	replayButton();
+}
+
+// turn on or off game modal
+function toggleModal() {
+	let gameModal = document.querySelector('.modal-background');
+	gameModal.classList.toggle('hide');
 }
 
 function restartGame() {
-	let restartButton = document.querySelector('.restart');
-	restartButton.addEventListener('click', resetGameState);
+	resetGameState();
+	toggleModal();
+}
+
+// return game from showing game modal to initial state 
+function replayButton() {
+	document.querySelector('.modal-button-replay').addEventListener('click', restartGame);
+}
+
+// stop time progression and show game modal
+function gameWon() {
+	clearInterval(watchId);
+	fillModalContent();
+	toggleModal();
 }
 
 function startGame() {
 	generateCards();
-	initClick();
-	restartGame();
+	initCards();
+	restartButton();
 }
 
 startGame();
